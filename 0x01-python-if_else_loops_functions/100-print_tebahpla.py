60e1f923c01bddcf94692aaeb253098d1552ff58
cheiy <gschege@gmail.com>
2023-01-05 18:26:12 +0000

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,12 +5,11 @@ lower = 122
 i = 1
 while i <= 26:
     if ((x % 2) == 0):
-        print("{}".format(chr(lower)), end="")
-        lower -= 1
-        upper -= 1
+        letter = lower
     else:
-        print("{}".format(chr(upper)), end="")
-        upper -= 1
-        lower -= 1
+        letter = upper
+    print("{}".format(chr(letter)), end="")
+    lower -= 1
+    upper -= 1
     x += 1
     i += 1
