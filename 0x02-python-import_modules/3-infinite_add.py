463ad4aece5edfae587aed8e4694fad2a42467c0
cheiy <gschege@gmail.com>
2023-01-05 22:34:05 +0000

    Infinite Add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     sum = 0
     args = len(sys.argv)
     if args == 1:
-        print("{}".format(0))
+        sum = 0
     else:
         for i in range(args):
             if i != 0:
