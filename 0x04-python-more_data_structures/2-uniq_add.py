3915b8186a33bdcae4577fe1cd9edb36f22d9c49
cheiy <gschege@gmail.com>
2023-01-11 18:35:26 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     result = dups_sum = uniq_sum = 0
+    if my_list is None:
+        exit()
     for item in my_list:
         if my_list.count(item) > 1:
             dups_sum += item
