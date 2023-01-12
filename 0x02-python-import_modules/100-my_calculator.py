8dc16b3d36913b94aa4542be168fe30d0ee319b3
cheiy <gschege@gmail.com>
2023-01-12 16:27:00 +0000

    Correction of my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,18 @@ import sys
 
 if __name__ == "__main__":
     args = len(sys.argv)
-    if args < 3:
+    if args < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        operator = sys.argv[2]
+        if len(sys.argv[2]) > 1:
+            print(sys.argv[2])
+            operator = "*"
+            b = sys.argv[3]
+            print(operator)
+            print(b)
+        else:
+            operator = sys.argv[2]
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if operator == "+":
