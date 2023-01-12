0881634b56e6bb09fdb37eb6a247713ccd1aa43d
cheiy <gschege@gmail.com>
2023-01-12 00:44:49 +0000

    Best score - correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,10 +6,10 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return (None)
     for key in a_dictionary:
-        if key is None:
-            return (None)
         num = a_dictionary.get(key)
         if num > biggest:
             biggest = num
             biggest_key = key
+    if a_dictionary.get(biggest_key) is None:
+        return (None)
     return (biggest_key)
