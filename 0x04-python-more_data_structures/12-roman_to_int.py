45a42d79b825f317e91606edd2ea31d9031a3b6d
cheiy <gschege@gmail.com>
2023-01-12 02:05:51 +0000

    Roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    num = 0
     type_ = type(roman_string)
-    if 'str' not in type_ and type_ is not None:
+    if type_ is None:
         return (0)
+    dict_ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for letter in roman_string:
+        num += dict_ro.get(letter)
+    return (num)
