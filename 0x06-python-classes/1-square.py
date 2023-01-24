bb55876fbd5040fb441d45c64a59707725ea14cd
cheiy <gschege@gmail.com>
2023-01-24 19:49:30 +0000

    Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module houses one class that defines a square."""
+
+
+class Square():
+    """Class Square defines a square"""
+
+    def __init__(self, size=None):
+        """Init method that instantiates __size__ with None
+
+
+        Args:
+            size (:obj: `int`): The size of the square.
+
+        """
+        self.__size = None
