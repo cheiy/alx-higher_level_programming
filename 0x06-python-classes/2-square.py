eea17b3c1bd8ba765e8247b96aa93784a0b6e185
cheiy <gschege@gmail.com>
2023-01-24 21:19:19 +0000

    Square 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module houses one class that defines a square."""
+
+
+class Square():
+    """Class Square defines a square"""
+    def __init__(self, size=0):
+        """Init method that instantiates __size__ with None
+
+
+        Args:
+            size (:obj: `int`): The size of the square.
+
+        """
+        try:
+            if isinstance(size, int) is False:
+                raise TypeError
+            if size < 0:
+                raise ValueError
+        except TypeError:
+            print("{}".format("size must be an integer"))
+        except ValueError:
+            print("{}".format("size must be >= 0"))
+        else:
+            self.__size = size
