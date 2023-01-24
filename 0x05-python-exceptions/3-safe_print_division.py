ba59a773117e5682acef12ed3bb8d9e85d0ca6c9
cheiy <gschege@gmail.com>
2023-01-24 13:15:47 +0000

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        result = None
+    finally:
+        return result
