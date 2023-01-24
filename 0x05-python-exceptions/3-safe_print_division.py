cf7e74e9507b3d3eb0d99d3680a4dca311aa6f84
cheiy <gschege@gmail.com>
2023-01-24 13:31:49 +0000

    Safe_Print_Division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,11 @@ def safe_print_division(a, b):
     except TypeError:
         pass
     except ZeroDivisionError:
+        print("{}".format("Inside result: None"))
         result = None
     finally:
-        return result
+        try:
+            print("{} {:.1f}".format("Inside result: ", result))
+        except TypeError:
+            pass
+    return result
