8687074c331ae19a9b76aef8940f5d7c6f0d388e
cheiy <gschege@gmail.com>
2023-01-30 18:51:04 +0000

    1 rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Module houses the Rectangle class"""
+
+
+class Rectangle:
+    """This is a class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ Init method initializes width and height to 0
+
+        Args:
+            width (int): Initializes the width of the rectangle.
+            height (int): Initializes the height of the rectangle.
+
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """int: Gets the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """See getter method above for this setter"""
+        try:
+            if isinstance(value, int) is False:
+                raise TypeError('width must be an integer')
+            if value < 0:
+                raise ValueError('width must be >= 0')
+            self.__width = value
+        except TypeError as err:
+            print(err)
+        except ValueError as err:
+            print(err)
+
+    @property
+    def height(self):
+        """int: Gets the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set getter method above for this setter"""
+        try:
+            if isinstance(value, int) is False:
+                raise TypeError('height must be an integer')
+            if value < 0:
+                raise ValueError('height must be >= 0')
+            self.__height = value
+        except TypeError as err:
+            print(err)
+        except ValueError as err:
+            print(err)
