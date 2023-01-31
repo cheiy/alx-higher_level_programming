d205352f39b20b0c8df83c2eb9f8507b30d653ce
cheiy <gschege@gmail.com>
2023-01-31 20:10:15 +0000

    Unittests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        """ Function tests whether the max integer is returned"""
+
+        list_1 = [1, 2, 3, 4, 5, 6, -10]
+        list2 = [-1, -2, -3, 0]
+        self.assertEqual(max_integer(list_1), 6)
+        self.assertEqual(max_integer(list2), 0)
+
+    def test_types(self):
+        """ Function tests whether the max_integer function
+        raises an error when wrong types are fed into it"""
+
+        list_1 = ['some', 'string', 5, 6, 7]
+        list_2 = [6.1, 5.4, -8.76, -9.09]
+        self.assertRaises(TypeError, max_integer, list_1)
+        self.assertRaises(TypeError, max_integer, list_2)
