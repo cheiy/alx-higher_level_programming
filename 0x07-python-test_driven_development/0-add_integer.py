9b1838bd9dcd77a03c63b680017909ce37817dd3
cheiy <gschege@gmail.com>
2023-02-04 20:11:12 +0000

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+""" Module houses the add_integer function """
+
+
 def add_integer(a, b=98):
     """Function adds 2 integers"""
 
     try:
         if isinstance(a, (float, int)) is False:
             raise TypeError('a must be an integer')
+        else:
+            a = int(a)
         if isinstance(b, (float, int)) is False:
             raise TypeError('b must be an integer')
-        if isinstance(a, float) is True:
-            a = int(a)
-        if isinstance(b, float) is True:
+        else:
             b = int(b)
         return (a + b)
     except TypeError as err:
         print(err)
+        exit()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
