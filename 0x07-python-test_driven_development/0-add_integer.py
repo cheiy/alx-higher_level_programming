ad62dac15d0fb853dd03f625af923339a59baddd
cheiy <gschege@gmail.com>
2023-02-04 20:11:12 +0000

    Correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,18 +6,18 @@ def add_integer(a, b=98):
     """Function adds 2 integers"""
 
     try:
-        if isinstance(a, (float, int)) is False:
+        if isinstance(a, (float, int)) is False or a is None:
             raise TypeError('a must be an integer')
         else:
             a = int(a)
-        if isinstance(b, (float, int)) is False:
+        if isinstance(b, (float, int)) is False or b is None:
             raise TypeError('b must be an integer')
         else:
             b = int(b)
-        return (a + b)
     except TypeError as err:
         print(err)
-        exit()
+    else:
+        return (a + b)
 
 
 if __name__ == "__main__":
