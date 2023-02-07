2353001c1a0af154df5eb8554b0c7c61c19bc33a
cheiy <gschege@gmail.com>
2023-02-07 01:06:12 +0000

    Added __init__

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,6 +5,10 @@
 class BaseGeometry:
     """ BaseGeometry class """
 
+    def __init__(self, name="", value=0):
+        self.name = name
+        self.value = value
+
     def area(self):
         """ Area method """
         raise Exception('area() is not implemented')
