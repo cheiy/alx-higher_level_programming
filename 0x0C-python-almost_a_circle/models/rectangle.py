ac07d3e8c5e7939ae2f6cd2d14ffa1a2040c57ee
cheiy <gschege@gmail.com>
2023-02-13 23:53:33 +0000

    Checked

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-""" Module contains the Rectange class which inherits
-from the Base class"""
+""" Module contains the Base class """
 
 
-from models import Base
+class Base:
+    """ Defines the Base Class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Class constructor method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
 
 class Rectangle(Base):
-    """ The Rectangle class """
+    """ Defines the Rectangle class which inherits from the Base class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Class constructor method"""
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -19,40 +27,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Gets the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, width):
-        """Sets the width of the rectangle"""
+        if isinstance(width, int) is False:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
-    def height(self, width):
-        """Gets the height of the rectangle"""
-        return self.__width
+    def height(self):
+        return self.__height
 
     @height.setter
     def height(self, height):
-        """Sets the height of the rectangle"""
+        if isinstance(height, int) is False:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
     def x(self):
-        """Gets x"""
         return self.__x
 
     @x.setter
     def x(self, x):
-        """Sets x"""
+        if isinstance(x, int) is False:
+            raise TypeError("x must be an integer")
+        if x <= 0:
+            raise ValueError("x must be > 0")
         self.__x = x
 
     @property
     def y(self):
-        """Gets y"""
         return self.__y
 
     @y.setter
     def y(self, y):
-        """Sets the value of y"""
+        if isinstance(y, int) is False:
+            raise TypeError("y must be an integer")
+        if y <= 0:
+            raise ValueError("y must be > 0")
         self.__y = y
