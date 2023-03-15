4e1ed575169d6db43c128fcbd8afb6b65e71b390
cheiy <gschege@gmail.com>
2023-03-15 17:51:09 +0000

    States

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- Script creates the database hbtn_0d_usa and the table states in the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.states(id INT UNIQUE PRIMARY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.states(id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
 ALTER TABLE `hbtn_0d_usa`.`states` ALTER COLUMN name DROP DEFAULT;
