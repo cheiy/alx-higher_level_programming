c3f5a22a057be9e8f3cb6962dc878a2faa0fabff
cheiy <gschege@gmail.com>
2023-03-15 01:21:35 -0700

    Create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script creates user user_0d_1@localhost and grants all privileges on the SQL server.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
