8f3951d7b00fb34f624e4ae7ba6805de02576720
cheiy <gschege@gmail.com>
2023-03-15 19:13:24 +0000

    Cities

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
---Script lists all the cities of California that can be found in the hbtn_0d_usa db
-SELECT * FROM cities WHERE state_id=1 ORDER BY id;
+-- Script lists all the cities of California that can be found in the hbtn_0d_usa db
+SELECT * FROM cities WHERE state_id=1 ORDER BY cities.id ASC;
