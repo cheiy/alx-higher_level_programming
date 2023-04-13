625fb468caa63340e52247ed0e818d1a712ea4e0
cheiy <gschege@gmail.com>
2023-04-13 22:12:56 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = [];
+array[0] = 'C is fun';
+array[1] = 'Python is cool';
+array[2] = 'JavaScript is amazing';
+for (let i = 0; i < 3; i++) {
+  console.log(array[i]);
+}
