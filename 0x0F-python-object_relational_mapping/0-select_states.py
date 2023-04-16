756b05963d0f572449dd1a3642b05aefad1aaad5
cheiy <gschege@gmail.com>
2023-04-16 15:57:46 +0000

    multiline indentation correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", user="root", port=3306, passwd="roo\
+                         t", db="hbtn_0e_0_usa")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     result = cur.fetchall()
