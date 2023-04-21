93fd0570b470c6f0779e4c44c347f97537fd0d58
cheiy <gschege@gmail.com>
2023-04-21 17:11:55 +0000

    Module documentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,16 +2,17 @@
 """
 Script lists all State objects from hbtn_0e_6_usa
 """
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-db_user = argv[1]
-db_pass = argv[2]
-db_name = argv[3]
 
 if __name__ == "__main__":
+    db_user = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(db_user,
                            db_pass, db_name))
     Base.metadata.create_all(engine)
