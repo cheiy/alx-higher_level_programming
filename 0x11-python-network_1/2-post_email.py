aaf84615e6821873779862aba4c030320e4174dd
cheiy <gschege@gmail.com>
2023-04-27 23:06:59 +0000

    Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,9 +11,10 @@ if __name__ == "__main__":
 
     url = argv[1]
     email_address = argv[2]
-    data = urllib.parse.urlencode(email_address)
+    '''data = urllib.parse.urlencode(email_address)
     data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
+    '''
+    req = urllib.request.Request(url, email_address)
     with urllib.request.urlopen(req) as response:
         our_data = response.read()
     print(our_data)
