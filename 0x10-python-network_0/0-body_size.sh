59d4c521fee800b96633c91028824563c067aa50
cheiy <gschege@gmail.com>
2023-04-27 18:52:55 +0000

    cURL -w option used

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Bash script takes in a URL, sends request to that URL and displays the size of the body of the response
 URL="$1"
-curl -sGI "$URL" | grep "Content-Length" | cut -d " " -f 2
+#curl -sGI "$URL" | grep "Content-Length" | cut -d " " -f 2
+curl -sG "$URL" -w '%{size_download}\n' | tail -n 1
