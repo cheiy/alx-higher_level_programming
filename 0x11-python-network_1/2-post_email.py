0c595c2cf0b316617f9ae270e5d8b57455fdefbe
cheiy <gschege@gmail.com>
2023-04-28 22:45:43 +0000

    Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,13 +8,13 @@ the body response (decode in utf-8)
 if __name__ == "__main__":
     from sys import argv
     import urllib.request
+    import urllib.parse
 
     url = argv[1]
     email_address = argv[2]
-    '''data = urllib.parse.urlencode(email_address)
+    data = urllib.parse.quote(email_address)
     data = data.encode('ascii')
-    '''
-    req = urllib.request.Request(url, email_address)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         our_data = response.read()
     print(our_data)
