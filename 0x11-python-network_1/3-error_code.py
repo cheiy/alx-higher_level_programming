4c6a7905892d176bbe2e664c73cbb091d3d9d5f1
cheiy <gschege@gmail.com>
2023-04-29 20:37:03 +0000

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+This script takes in a URL, sends a request to the URL and displays the body
+of the response (decoded in utf-8)
+'''
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import urllib.error
+    from sys import argv
+
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            content = response.read().decode('utf-8')
+    except urllib.error.HTTPError as e:
+        print("Error Code: {}".format(e.code))
+    else:
+        print(content)
