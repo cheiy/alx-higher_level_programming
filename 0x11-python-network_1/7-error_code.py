75334eaa4b6fc98fdfc9731ed2a40eacef90db3b
cheiy <gschege@gmail.com>
2023-04-29 22:59:49 +0000

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Script takes in a URL, sends a request to the URL and displays the body
+of the response
+'''
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
