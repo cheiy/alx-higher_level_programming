defe574e07ebe9b28331155ed14ed040e860006f
cheiy <gschege@gmail.com>
2023-04-29 21:47:52 +0000

    What's my status?

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Script fetches https://alx-intranet.hbtn.io/status using the requests package
+'''
+
+if __name__ == "__main__":
+    import requests
+
+    url = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(url)
+    print("Body response:")
+    print("- type: {}".format(type(r.reason)))
+    print("- content: {}".format(r.text))
