49afb39638c088b6de81c9f7dd5f778a3e7959e1
cheiy <gschege@gmail.com>
2023-05-01 13:46:09 +0000

    Finally!

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script displays all HTTP methods accepted by server
-curl -s -IL -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2
+curl -s -IL -X OPTIONS "$1" | grep "Allow" | cut -d " " -f 2-
