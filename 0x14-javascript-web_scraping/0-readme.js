4fcaf85de36d67c8f4626955f3469e9f33706307
cheiy <gschege@gmail.com>
2023-05-23 12:21:00 +0000

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
